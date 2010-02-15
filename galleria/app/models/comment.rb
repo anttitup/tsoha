3896fa7fda7b2eddaca408553ee76e715bf1bfdd
Unknown <tupis@antin.(none)>
2010-02-16 00:10:06 +0200

    koodausta

--- galleria/app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :image
-  validates_presence_of :comment 
-end
--- galleria/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-  has_many:comment
-  has_many:image
-  validates_presence_of :name,:email,:pasworld
-end
