e3ead74d51600f597bc37081c14b8f4b438b0c18
Unknown <tupis@antin.(none)>
2010-02-26 21:17:03 +0200

    ajoin migraatiot

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100226184519) do
+ActiveRecord::Schema.define(:version => 20100226191120) do
 
   create_table "comments", :force => true do |t|
     t.text     "comment"
